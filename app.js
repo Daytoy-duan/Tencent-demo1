0f106cc9d013c88c57cbdff6930d071af3122300
Daytoy-duan <2460673718@qq.com>
2022-07-09 16:07:02 +0800

    Tencent-demo1
    页面布局

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 App({
   onLaunch() {
+    //生命周期
+    console.log("这是onLaunch方法")
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -12,6 +14,12 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
+  },
+  onShow() {
+    console.log("这是onShow方法")
+  },
+  onHide() {
+    console.log("这是onHide方法")
   },
   globalData: {
     userInfo: null
